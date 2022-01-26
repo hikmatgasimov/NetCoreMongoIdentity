09bc83c41da20eda7f42fc6a7ce67ad9e2451be0
gasimovhikmat <gasimov.hikmat95@gmail.com>
2022-01-26 17:57:41 +0400

    initial

---
+++ Downloads/loans/loans/src/main/java/com/synbytes/loans/controller/LoansController.java
@@ -0,0 +1,29 @@
+package com.synbytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.synbytes.loans.model.Customer;
+import com.synbytes.loans.model.Loans;
+import com.synbytes.loans.repository.LoansRepository;
+
+@RestController
+public class LoansController {
+	@Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+}
